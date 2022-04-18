779659ad5cd0d65e89292dbb31d469dafd25e954
Chiragroop <chirag.kahlon@gmail.com>
2022-04-17 19:21:41 -0700

    * Explained how to use ddr_error_mapping in README * Improved error messages * Added help and version flags * Code refactoring

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ddrescue_error_mapping
-This is a simple tool that converts ddrescue map files to dmintegrity files allowing you to use the bad sector maps on any data recovery tools that run under Linux.
+This is a simple tool that converts ddrescue map files to dmintegrity table allowing you to use the bad sector maps on any data recovery tools that run under Linux (Current testing shows that this does *not* work under WSL). To create error mapping, you need both an image and the ddrescue map file. While ddrescue is the obvious tool that can create both, other tools can also create ddrescue compatible map files (like HDDSuperClone)
+
+## Usage
+Once you have an image, mount the image. Make sure to get the sector size (the default for mounting images is 512 bytes)
+```
+sudo losetup <device location ex: /dev/loop50> <path to image file> -r -b=[512/4096]
+```
+
+This will create a block device at `<device location>` with the specified sector size in `-b` parameter. The `-r` parameter ensures the image is mounted read-only.
+
+Once you have an image, you can use the ddr_error_mapping by running 
+```
+ddr_error_mapping <ddrescue log file> <path of block device> >mapping.txt
+```
+Note the block device is the block device created with losetup. This command will create the necessary mapping for dmsetup. Finally, run `sudo cat <path to mapping> | sudo dmsetup create <device name>`
+
+This will create a new device at `/dev/mapper/<device name>` which you can use with tools. Tested with DMDE (for faster recovery, set retries to 0), UFS Explorer and R-Studio
+
+## Install
+ddr_error_mapping install binaries are available at [Releases](https://github.com/CKingX/ddrescue_error_mapping/releases) page for Windows and Linux binaries (x64 architecture only). I currently do not have a Mac to test ddr_error_mapping with, but this should work regardless (Note that ddr_error_mapping will only create the device mapping file. You still need Linux to create the device mapper block device)
+
+## Build Guide
+Use cargo to build ddr_error_mapping. Currently tested with rustc version 1.60
+
+## Limitations
+* `dmsetup` error device did not work under WSL in my testing
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,47 @@
 use text_io::scan;
 use std::{env, fs, process};
 
-static CONVERT_ERROR: &str = "Could not convert hex to decimal";
-static FILE_ERROR: &str = "You must put the path to map file";
-static DEVICE_ERROR: &str = "No device path given";
+const CONVERT_ERROR: &str = "Could not convert hex to decimal";
+const NO_PATH: &str = "No path to file given";
+const DEVICE_ERROR: &str = "No device path given";
+const HELP: &str = "ddr_error_mapping <ddrescue log> <device path> [sector size (512 default)]\n
+--help -h Shows this page\n
+--version -v Shows the version";
+const FILE_ERROR: &str = "Unable to either open or parse file";
+const VERSION: &str = "ddr_error_mapping  0.5.0";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.iter().count() < 3 {
-        println!("ddrescue_error_mapping <ddrescue log> <device path>");
+        if let Some(arg) = args.iter().nth(1) {
+            if arg.contains("--version") || arg.contains("-v") {
+                println!("{VERSION}");
+            }
+        }  else {
+            println!("{}", HELP);
+        }
         process::exit(0);
     }
     let filename;
     let device_name;
-    {
-        let mut options = args.into_iter();
-        filename = options.nth(1).expect(FILE_ERROR);
-        device_name = options.nth(0).expect(DEVICE_ERROR);
-    }
+
+    let mut options = args.into_iter();
+    filename = options.nth(1).expect(NO_PATH);
+    device_name = options.nth(0).expect(DEVICE_ERROR);
     
     let contents= fs::read_to_string(filename)
-                    .unwrap_or_else(|_| panic!("File may be corrupted"));
+                    .unwrap_or_else(|_| panic!("{FILE_ERROR}"));
 
     let mut file_line = contents.lines()
                         .filter(|s| !s.is_empty() && !s.contains("#"))
                         .map(|s| s.trim())
                         .skip(1);
 
-    let mut output: Vec<String> = Vec::new();
+    let mut output: Vec<String> = if let (_, Some(capacity)) = file_line.size_hint() {
+        Vec::with_capacity(capacity)
+    } else {
+        Vec::new()
+    };
 
     while let Some(line) = file_line.next() {
         let pos_string: String;
@@ -47,13 +62,13 @@ fn main() {
         }
     }
 
-    output.into_iter().for_each(|n| println!("{}", n));
+    output.into_iter().for_each(|n| println!("{n}"));
 }
 
-    fn create_error(pos: u64, size: u64) -> String {
-        format!("{} {} error", pos/512, size/512)
-    }
+fn create_error(pos: u64, size: u64) -> String {
+    format!("{} {} error", pos/512, size/512)
+}
 
-    fn create_linear(pos: u64, size: u64, device: &str) -> String {
-        format!("{} {} linear {} {}", pos/512, size/512, device, pos/512)
-    }
+fn create_linear(pos: u64, size: u64, device: &str) -> String {
+    format!("{} {} linear {device} {}", pos/512, size/512, pos/512)
+}
